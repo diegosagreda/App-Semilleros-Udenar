7a5ae3d8c687e7e432379b023ed82884734feafe
Karen Mutis <kamo.9595@gmail.com>
2023-08-24 17:23:53 -0500

    evento-reportesPDF

--- resources/views/content/pages/eventos/pages-eventos-reportes.blade.php
+++ resources/views/content/pages/eventos/pages-eventos-reportes.blade.php
@@ -1,60 +1,67 @@
-<body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reporte de Evento</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@2.0.7/css/boxicons.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
-        /* Agrega tus estilos aquí */
         body {
             font-family: Arial, sans-serif;
-            margin: 0; /* Agrega margen cero para asegurarte de que no haya espacios innecesarios */
+            background-color: #f4f4f4;
+            margin: 0;
         }
-        h1 {
-            color: #333;
-            font-size: 24px;
-            text-align: center;
-        }
-        /* Agrega más estilos según tus necesidades */
         .event-details {
-            margin-left: 20px; /* Agrega margen a la izquierda para mover los detalles del evento */
+            background-color: #fff;
+            border: 1px solid #ccc;
+            border-radius: 10px;
+            margin: 20px auto;
+            padding: 20px;
+            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+            max-width: 600px;
         }
-        .user-profile-img {
+        .table th {
             width: 150px;
-            height: 150px;
-            border-radius: 50%;
+            border: 1px solid #ccc;
+            background-color: #f9f9f9;
         }
-        .list-unstyled {
-            list-style: none; /* Quitar los puntos de la lista */
-            padding: 0; /* Quitar el relleno de la lista */
-        }
-        .list-unstyled li {
-            margin-bottom: 10px; /* Agregar espacio entre elementos de la lista */
-        }
-        .list-unstyled li i {
-            margin-right: 10px; /* Espacio entre el icono y el texto en la lista */
+        .table td {
+            border: 1px solid #ccc;
         }
     </style>
-    <h1>Reporte de Evento</h1>
-    <div class="event-details">
-        <p class="fw-semibold mx-2" style="font-size: 25px; ">DETALLES DEL EVENTO</p>
-        <ul class="list-unstyled mb-4 mt-3">
-            <li class="d-flex align-items-center">
-                <i class="bx bx-key" style="color: rgb(160, 156, 37)"></i>
-                <span class="fw-semibold mx-2">No#:</span>
-                <span>{{$evento->codigo}}</span>
-            </li>
-            <li class="d-flex align-items-center">
-                <i class="bx bx-calendar-alt" style="color: rgb(203, 45, 45)"></i>
-                <span class="fw-semibold mx-2" class="fw-semibold mx-2" style="font-size: 20px; ">Evento:</span>
-                <span style="">{{$evento->nombre}}</span><br><br>
-                <span class="fw-semibold mx-2" class="fw-semibold mx-2" style="font-size: 20px; ">Fecha Inicio:</span>
-                <span>{{$evento->fecha_inicio}}</span><br><br>
-                <span class="fw-semibold mx-2" class="fw-semibold mx-2" style="font-size: 20px; ">Fecha Fin:</span>
-                <span>{{$evento->fecha_fin}}</span><br><br>
-                <span class="fw-semibold mx-2" style="font-size: 20px; ">Descripción:</span>
-                <span>{{$evento->descripcion}}</span><br>
-                {{-- <span class="fw-semibold mx-2">Nombre Proyecto:</span>
-                <span>{{$proyecto->nomProyecto}}</span> --}}
-            </li>
-            <!-- Agrega los demás elementos de la lista -->
-        </ul>
-        {{-- <img alt="Event Image" class="d-block h-auto ms-0 ms-sm-4 rounded-3 user-profile-img" src="{{ asset('public/assets/eventos/' . $evento->foto) }}" alt="Foto del evento"> --}}
+</head>
+<body>
+    <p class="text-right">Fecha del Reporte: {{ date('d/m/Y') }}</p>
+    <div class="container">
+        <div class="event-details">
+            
+            <h2>Detalles Evento</h2>
+            <table class="table">
+                <tbody>
+                    <tr>
+                        <th>No#</th>
+                        <td>{{$evento->codigo}}</td>
+                    </tr>
+                    <tr>
+                        <th>Evento</th>
+                        <td>{{$evento->nombre}}</td>
+                    </tr>
+                    <tr>
+                        <th>Fecha Inicio</th>
+                        <td>{{$evento->fecha_inicio}}</td>
+                    </tr>
+                    <tr>
+                        <th>Fecha Fin</th>
+                        <td>{{$evento->fecha_fin}}</td>
+                    </tr>
+                    <tr>
+                        <th>Descripción</th>
+                        <td>{{$evento->descripcion}}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
 </body>
+</html>
